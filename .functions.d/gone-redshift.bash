4770d9d911de10d655dc5e4c23158de9f8bbf21d
Iñaki Abete <inakiabt@gmail.com>
2015-10-08 16:43:06 -0300

    Add gone user changes function

--- .functions.d/gone-redshift.bash
+++ .functions.d/gone-redshift.bash
@@ -63,3 +63,36 @@ gone_product_changes() {
 
     echo $outputFile
 }
+
+gone_user_changes() {
+    if [ -z "$1" ]; then
+        echo "profile is required"
+        exit 1
+    fi
+
+    if [ -z "$2" ]; then
+        echo "user id is required"
+        exit 1
+    fi
+
+    profile=${1//\//}
+    profilePath=~/.config/profiles/redshift/.$profile
+    userId=$2
+
+    if [ ! -f "$profilePath" ]; then
+        echo "profile $profile does not exist"
+        exit 1
+    fi
+    source $profilePath
+
+    query="select * from raw_user where object_id='$userId' order by updated_at desc;"
+
+    outputPath=~/tmp/redshift
+    outputFile=$outputPath/user-$userId.html
+
+    [ ! -d "$outputPath" ] && mkdir -p $outputPath
+
+    psql -p 5439 -h $REDSHIFT_HOST -U $REDSHIFT_USER -d $REDSHIFT_DB -W -c "$query" -H > $outputFile
+
+    echo $outputFile
+}
