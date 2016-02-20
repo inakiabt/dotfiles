efea707034cf7102d9a6b0c789a619a11e05a4dd
Iñaki Abete <inakiabt@gmail.com>
2016-02-19 23:55:55 -0300

    Add dockerfile linter

--- .aliases.d/docker.bash
+++ .aliases.d/docker.bash
@@ -2,3 +2,7 @@ alias dockercleancontainers="docker ps -aq | xargs docker rm"
 alias dockercleanimages="docker images -aq -f dangling=true | xargs docker rmi"
 alias dockerclean="dockercleancontainers && dockercleanimages && clean_docker"
 alias docker-killall="docker ps -q | xargs docker kill"
+
+# lint a Dockerfile: https://github.com/lukasmartinelli/hadolint/
+alias hadolint="docker run --rm -i lukasmartinelli/hadolint"
+alias dockerfile-lint="hadolint"
